ed7630c8c21cdb48a09c28e2ab0d81dc18130b39
openly-retro <3236094+openly-retro@users.noreply.github.com>
2024-02-01 10:41:26 -0500

    Suppress warnings about invalid escape sequences

--- mre_code_tools/stellaris_yaml_converter.py
+++ mre_code_tools/stellaris_yaml_converter.py
@@ -6,20 +6,20 @@ from yaml import safe_load
 
 def convert_stellaris_script_to_standard_yaml(input_string):
     # NOT aren't used in trait description anyway
-    convert_tabs_to_spaces = re.sub('\t', '  ', input_string)
-    remove_comment_blocks = re.sub('\n#.*', '', convert_tabs_to_spaces)
-    comment_nots = re.sub('NOT', '#NOT', remove_comment_blocks)
-    convert_blocks = re.sub('(?<!NOT) = \{\n', ':\n', comment_nots)
-    convert_sameline_blocks = re.sub('(?<!NOT) = \{', ':', convert_blocks)
-    remove_closing_braces = re.sub('\s*\}\n', '\n', convert_sameline_blocks)
+    convert_tabs_to_spaces = re.sub(r"\t", '  ', input_string)
+    remove_comment_blocks = re.sub(r"\n#.*", '', convert_tabs_to_spaces)
+    comment_nots = re.sub("NOT", '#NOT', remove_comment_blocks)
+    convert_blocks = re.sub(r"(?<!NOT) = \{\n", ':\n', comment_nots)
+    convert_sameline_blocks = re.sub(r"(?<!NOT) = \{", ':', convert_blocks)
+    remove_closing_braces = re.sub(r"\s*\}\n", '\n', convert_sameline_blocks)
     create_key_value_pairs = re.sub(' = ', ': ', remove_closing_braces)
-    clean_closing_brace_no_newline = re.sub('\s*\}', '', create_key_value_pairs)
-    remove_extralines = re.sub('\n{2,}', '\n', clean_closing_brace_no_newline)
+    clean_closing_brace_no_newline = re.sub(r"\s*\}", '', create_key_value_pairs)
+    remove_extralines = re.sub(r"\n{2,}", '\n', clean_closing_brace_no_newline)
     # One-liners with multiple nested {} don't cleanly replace, so comment those lines
     # since we are doing traits anyway.. dont need those lines
-    comment_nested_multiline = re.sub('\n(\s)(?=((.*:){2,4}))', '\n#', remove_extralines)
+    comment_nested_multiline = re.sub(r"\n(\s)(?=((.*:){2,4}))", '\n#', remove_extralines)
     # leftover_multiline_comment = re.sub('#{2,}', '', comment_nested_multiline)
-    comment_out_variables = re.sub('\@', 'var_', comment_nested_multiline)  # YAML doesn't like the @ symbol
+    comment_out_variables = re.sub(r"\@", 'var_', comment_nested_multiline)  # YAML doesn't like the @ symbol
     # Carefully go through leader_class definitions
     # TODO: Fix bug where list of fewer elements is patched before long list
     structured_leader_class_lists3 = convert_leader_class_definitions_to_lists(comment_out_variables, 3)
@@ -32,27 +32,6 @@ def convert_leader_class_definitions_to_lists(input_string, min_classes_length: 
     # 'leader_class: ["commander", "official"]' which is valid yaml
     # min_classes_length means crunch N number of sequential leader class names into a list.
     leader_class_query = re.compile(f"(leader_class:(\s(commander|official|scientist)){{{min_classes_length},}})")
-    results = re.findall(leader_class_query, input_string)
-    # we get a list of tuples like [('leader_class: commander official scientist', ' scientist', 'scientist'),
-    # ('leader_class: commander official', ' official', 'official'), etc
-    input_string_copy = input_string
-    for result in results:
-        complete_line = result[0]
-        parts = complete_line.split(': ')
-        unformatted_classes = parts[1]
-        classes_as_list = unformatted_classes.split(' ')
-        # now we have ["commander", "official", "scientist"]
-        # the trick is directly insert the str version of the list
-        # breakpoint()
-        line_with_structured_data = f"{parts[0]}: {str(classes_as_list)}"
-        # oh the horror
-        input_string_copy = re.sub(complete_line, line_with_structured_data, input_string_copy)
-    return input_string_copy
-
-def convert_leader_class_definitions_to_lists_1(input_string):
-    # convert like 'leader_class: commander official' to
-    # 'leader_class: ["commander", "official"]' which is valid yaml
-    leader_class_query = re.compile("(leader_class:(\s(commander|official|scientist)){1,})")
     results = re.findall(leader_class_query, input_string)
     # we get a list of tuples like [('leader_class: commander official scientist', ' scientist', 'scientist'),
     # ('leader_class: commander official', ' official', 'official'), etc
@@ -107,19 +86,19 @@ if __name__=="__main__":
     if not args.infile:
         sys.exit('Need to specify an input file with --infile <filename>')
     sys.stdout.write('0xRetro Stellaris script chopper.. spinning up blades\n')
-
+    # breakpoint()
     with open(args.infile, "r") as infile:
         buffer = convert_stellaris_script_to_standard_yaml(
             infile.read()
         )
-    try:
-        _ = safe_load(buffer)
-    except Exception as ex:
-        sys.exit(f"There was a problem validating the YAML after chopping up the Stellaris script: {ex}")
-    if args.get('outfile'):
-        with open(args['outfile'], 'w+') as outfile:
-            outfile.write(buffer)
-    else:
-        print(buffer)
-    print("Done. There's a high change this finished correctly.")
-    sys.exit()
+        try:
+            _ = safe_load(buffer)
+        except Exception as ex:
+            sys.exit(f"There was a problem validating the YAML after chopping up the Stellaris script: {ex}")
+        if args.outfile:
+            with open(args.outfile, 'w+') as outfile:
+                outfile.write(buffer)
+        else:
+            print(buffer)
+        print("Done. There's a high change this finished correctly.")
+        sys.exit()
