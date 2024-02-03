04624c77ecefd7f6c852d4ce7bde9cb2d6b1dbca
openly-retro <3236094+openly-retro@users.noreply.github.com>
2024-02-03 11:55:55 -0500

    Make output more readable; slap on an author tag

--- mre_code_tools/run_mre_trait_pipeline.py
+++ mre_code_tools/run_mre_trait_pipeline.py
@@ -1,7 +1,15 @@
+#!/usr/bin/env python
+""" Load & transform base Stellaris traits files for use in the Machines & Robots Expansion mod """
+
+__author__ = "0xRetro (openly-retro)"
+__license__ = "GPLv3"
+__version__ = "1.0"
+
 import os
 from shutil import rmtree
 import sys
 from operator import itemgetter
+from random import randint
 
 import argparse
 from yaml import safe_load, safe_dump
@@ -19,11 +27,14 @@ BUILD_FOLDER = os.path.join(
     'build'
 )
 
+LEADER_CLASSES = [
+    "commander", "official", "scientist"
+]
+
 def clean_up_build_folder():
     if os.path.exists(BUILD_FOLDER):
         rmtree(BUILD_FOLDER)
-    else:
-        os.makedirs(BUILD_FOLDER)
+    os.makedirs(BUILD_FOLDER, exist_ok=True)
 
 def make_converted_filename(base_filename):
     without_ext = base_filename.split('.')[0]
@@ -62,7 +73,7 @@ def batch_process_base_files_into_yaml(stellaris_path: str) -> list:
         with open(target_converted_file_name, "w+") as dest_file:
             dest_file.write(buffer)
             sys.stdout.write(
-                f"Chopped up base file {base_file} successfully. Written to {dest_file}"
+                f"Chopped up base file {base_file} successfully. Written to {dest_file.name}\n"
             )
     return generated_files
 
@@ -93,18 +104,18 @@ def sort_merge_traits_files(useful_yaml_traits_files):
     for file in useful_yaml_traits_files:
         with open(file, "r") as input_file:
             buffer = safe_load(input_file.read())
-        for leader_class in ["official", "scientist", "commander"]:
+        for leader_class in LEADER_CLASSES:
             # breakpoint()
             output[leader_class] = output[leader_class] + buffer[leader_class]
 
     # Now, sort all traits per class
-    for leader_class in ["official", "scientist", "commander"]:
+    for leader_class in LEADER_CLASSES:
         # breakpoint()
         output[leader_class] = sorted(output[leader_class], key=lambda x: [*x][0]) 
 
     # Now, write each classes' traits to a file
 
-    for leader_class in ["official", "scientist", "commander"]:
+    for leader_class in LEADER_CLASSES:
         newfile_name = f"00_mre_{leader_class}_traits.yml"
         newfilepath = os.path.join(BUILD_FOLDER, newfile_name)
         with open(newfilepath, "w+") as traitsfile:
@@ -129,10 +140,22 @@ if __name__=="__main__":
             f"Couldnt find {args.stellaris_path}. Check that you entered the correct "
             "base path for Stellaris (the folder with \\common\\ in it)"
         )
-    clean_up_build_folder()
-    base_files_processed_to_yaml = batch_process_base_files_into_yaml(args.stellaris_path)
-    useful_traits_yaml_files = crunch_trait_data_from_processed_yaml(base_files_processed_to_yaml)
-    just_three_traits_files = sort_merge_traits_files(useful_traits_yaml_files)
     sys.stdout.write(
-        f"Let's see how we did! Inspect {just_three_traits_files} for errors."
+        "Starting the M&RE Trait Data pipeline, phase 1: Make trait data fun to play with!\n"
+        f"Code by 0xRetro. Sanity levels at {randint(15,115)}% of normal.\n"
+    )
+    sys.stdout.write("** Resetting the build folder **\n")
+    clean_up_build_folder()
+    sys.stdout.write("** Reading base files & chopping them up **\n")
+    base_files_processed_to_yaml = batch_process_base_files_into_yaml(args.stellaris_path)
+    sys.stdout.write("** Crunching & filtering chopped-up data **\n")
+    useful_traits_yaml_files = crunch_trait_data_from_processed_yaml(base_files_processed_to_yaml)
+    sys.stdout.write("** Sorting traits data & writing files **\n")
+    just_three_traits_files = sort_merge_traits_files(useful_traits_yaml_files)
+    sys.stdout.write("**** Mm mm, delicious, sane, predictable data! ****\n")
+    sys.stdout.write(
+        "Let's see how we did! Inspect these files in the build folder for errors:\n"
+    )
+    sys.stdout.write(
+        "\n".join([f"00_mre_{leader_class}_traits.yml" for leader_class in LEADER_CLASSES])
     )
