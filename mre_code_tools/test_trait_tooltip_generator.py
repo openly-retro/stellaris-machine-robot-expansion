41acdb817dd5eb9ae1a90a57e3f0aae4b0c94638
openly-retro <3236094+openly-retro@users.noreply.github.com>
2024-02-05 21:08:01 -0500

    Encode tooltip output during tests

--- mre_code_tools/test_trait_tooltip_generator.py
+++ mre_code_tools/test_trait_tooltip_generator.py
@@ -37,7 +37,31 @@ leader_trait_naturalist_3:
     trait_output = create_tooltip_for_leader(
         parsed_yaml, "official"
     )
-    expected_output = """xvcv_mdlc_leader_making_tooltip_official_leader_trait_naturalist_3:0 "§H$leader_trait_naturalist_machine$ III§!$add_xvcv_mdlc_leader_making_traits_costs_desc_alt$\n$governing_planet_effect$\n$mod_deposit_blockers_natural_unity_produces_add$: §G+12§!\n$governing_sector_effect$\n$mod_deposit_blockers_natural_unity_produces_add$: §G+6§!\n--------------\n§L$leader_trait_naturalist_machine_desc$§!"
+    expected_output = """
+#leader_making #official #leader_trait_naturalist_3
+xvcv_mdlc_leader_making_tooltip_official_leader_trait_naturalist_3:0 "§H$leader_trait_naturalist$ III§!$add_xvcv_mdlc_leader_making_traits_costs_desc_alt$\\n$governing_planet_effect$\\n$mod_deposit_blockers_natural_unity_produces_add$: §G+12§!\\n$governing_sector_effect$\\n$mod_deposit_blockers_natural_unity_produces_add$: §G+6§!\\n--------------\\n§L$leader_trait_naturalist_desc$§!"
 """
 
-    assert expected_output == trait_output
+    assert expected_output.encode('utf-8') == trait_output.encode('utf-8')
+
+def test_leader_trait_aggressive_2_fleet_modifier():
+    trait_data = {
+        "leader_trait_aggressive_2": {
+            "trait_name": "leader_trait_aggressive_2",
+            "leader_class": "commander",
+            "gfx": "GFX_leader_trait_aggressive",
+            "rarity": "common",
+            "fleet_modifier": {
+                "ship_fire_rate_mult": 0.05,
+                "ship_weapon_damage": 0.05
+            },
+            "requires_paragon_dlc": False
+        }
+    }
+    expected_output = """
+#leader_making #commander #leader_trait_aggressive_2
+xvcv_mdlc_leader_making_tooltip_commander_leader_trait_aggressive_2:0 "§H$leader_trait_aggressive$ II§!$add_xvcv_mdlc_leader_making_traits_costs_desc_alt$\\n$commanding_navy_effect$\\n$mod_ship_fire_rate_mult$: §G+5%§!\\n$mod_ship_weapon_damage$: §G+5%§!\\n--------------\\n§L$leader_trait_aggressive_desc$§!"
+"""
+    actual = create_tooltip_for_leader(trait_data, leader_class="commander")
+    # breakpoint()
+    assert expected_output.encode('utf-8') == actual.encode('utf-8')
--- mre_code_tools/trait_tooltip_generator.py
+++ mre_code_tools/trait_tooltip_generator.py
@@ -4,6 +4,8 @@ import sys
 from yaml import safe_load
 import argparse
 
+from mre_common_vars import TRAIT_MODIFIER_KEYS
+
 COLOR_CODES = {
     # https://steamcommunity.com/sharedfiles/filedetails/?id=1955978558
     "orange": "§H",
@@ -36,6 +38,22 @@ def make_brown_text(some_text):
 def make_green_text(some_text):
     return f"{COLOR_CODES['green']}{some_text}{CLOSE_CODE}"
 
+modifier_key_to_tooltip_prefix_var = {
+    "army_modifier": "commanding_army_effect",
+    "councilor_modifier": "councilor_trait",
+    "fleet_modifier": "commanding_navy_effect",
+    "modifier": "",
+    "planet_modifier": "governing_planet_effect",
+    "sector_modifier": "governing_sector_effect",
+    "self_modifier": "",
+    "triggered_army_effect": "commanding_army_effect",
+    "triggered_councilor_modifier": "councilor_trait",
+    "triggered_fleet_modifier": "commanding_navy_effect",
+    "triggered_planet_modifier": "governing_planet_effect",
+    "triggered_sector_modifier": "governing_sector_effect",
+    "triggered_self_modifier": ""
+}
+
 def create_tooltip_for_leader(
     trait_dict, leader_class, feature="leader_making"
 ):
@@ -47,8 +65,9 @@ def create_tooltip_for_leader(
     if trait_name[-1].isdigit():
         base_trait_name = trait_name.rsplit('_',1)[0]
         ending_num = trait_name.rsplit('_',1)[1]
-        trait_level = f" {DIGIT_TO_LATIN[ending_num]}"
-    trait_title = make_orange_text(f"${base_trait_name}_machine${trait_level}")
+        trait_level = f"{DIGIT_TO_LATIN[ending_num]}"
+    # trait_title = make_orange_text(f"${base_trait_name}_machine${trait_level}")
+    trait_title = make_orange_text(f"${base_trait_name}$ {trait_level}")
     tooltip_base = "xvcv_mdlc"
     if feature=="leader_making":
         tooltip_stem = "leader_making_tooltip"
@@ -56,28 +75,34 @@ def create_tooltip_for_leader(
 
     trait_cost_tt = "$add_xvcv_mdlc_leader_making_traits_costs_desc_alt$"
     separator_ruler = "--------------"
-    trait_desc_brown_text = make_brown_text(f"${base_trait_name}_machine_desc$")
+    # trait_desc_brown_text = make_brown_text(f"${base_trait_name}_machine_desc$")
+    trait_desc_brown_text = make_brown_text(f"${base_trait_name}_desc$")
 
+    root = trait_dict[trait_name]
     modifiers_list = []
-    if trait_dict[trait_name].get('planet_modifier'):
-        modifiers_list.append("$governing_planet_effect$")
-        for modifier in trait_dict[trait_name]['planet_modifier'].keys():
-            mod_tt_key = f"$mod_{modifier.lower()}$"
-            modified_amount = trait_dict[trait_name]['planet_modifier'][modifier]
-            modifiers_list.append(
-                f"$mod_{modifier.lower()}$: {make_green_text(f"+{modified_amount}")}"
-            )
-    if trait_dict[trait_name].get('sector_modifier'):
-        modifiers_list.append("$governing_sector_effect$")
-        for modifier in trait_dict[trait_name]['sector_modifier'].keys():
-            mod_tt_key = f"$mod_{modifier.lower()}$"
-            modified_amount = trait_dict[trait_name]['sector_modifier'][modifier]
-            modifiers_list.append(
-                f"$mod_{modifier.lower()}$: "
-                f"{make_green_text(f"+{modified_amount}")}"
-            )
-    trait_bonuses = "\n".join(modifiers_list)
-    return (
-        f"{tooltip_base}_{tooltip_stem}_{leader_class}_{trait_name}:0 \"{trait_title}{trait_cost_tt}\n"
-        f"{trait_bonuses}\n{separator_ruler}\n{trait_desc_brown_text}\"\n"
+    for modifier_key in TRAIT_MODIFIER_KEYS:
+        if root.get(modifier_key):
+            descriptive_prefix = f"${modifier_key_to_tooltip_prefix_var[modifier_key]}$"
+            modifiers_list.append(descriptive_prefix)
+            for modifier_name in root[modifier_key].keys():
+                mod_tt_key = f"$mod_{modifier_name.lower()}$"
+                modified_amount = root[modifier_key][modifier_name]
+                if "." in str(modified_amount) and str(modified_amount)[-1].isdigit():
+                    modified_amount = convert_decimal_to_percent_str(modified_amount)
+                modifiers_list.append(
+                    f"{mod_tt_key}: {make_green_text(f"+{modified_amount}")}"
+                )
+    trait_bonuses = '\\n'.join(modifiers_list)
+    compiled_tooltip = (
+        f'{tooltip_base}_{tooltip_stem}_{leader_class}_{trait_name}:0 \"{trait_title}{trait_cost_tt}\\n'
+        f'{trait_bonuses}\\n{separator_ruler}\\n{trait_desc_brown_text}\"'
     )
+    # breakpoint()
+    return f"""
+#{feature} #{leader_class} #{trait_name}
+{compiled_tooltip}
+"""
+
+def convert_decimal_to_percent_str(numeric_amount: float) -> str:
+    percent_float = numeric_amount * 100
+    return f"{int(percent_float)}%"
