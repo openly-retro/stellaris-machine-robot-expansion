2e6f1085efe4fa294ae5935110f49afbe4ea94be
Openly Retro <3236094+openly-retro@users.noreply.github.com>
2025-03-20 21:35:00 -0400

    Add all the json syntax during line crunching

--- mre_code_tools/cz2json/converter.py
+++ mre_code_tools/cz2json/converter.py
@@ -1,11 +1,13 @@
 import re
 
+"""
+Larger challenges:
+- separating multiple blocks on the same line
+- concatenating lists where items are individually on subsequent lines
+- Preserving the order of effects (not an issue in traits files)
+"""
 
-# expressions
-# line_opens_block
-# line is assignment only (no braces)
-# line is list assignment
-
+""" Regex Patterns """
 statement_left_side = re.compile(r"^\s{0,}(?P<assignee>\w*)\s{0,}=")
 parse_list_assignment = re.compile(
     r"(?P<assignee>\w*) = {(?P<items>.*)}"
@@ -21,18 +23,25 @@ re_single_word_line = re.compile(r"^\s{0,}(\w*){1}[ ]{0,}$")
 # in order to quote all words in the text blob
 nonnumber_words = re.compile(r"([a-zA-Z_\/]+)")
 # unquote "no" and "yes" afterwards
-detect_crunched_list = re.compile(r"(\w*): ")
+detect_crunched_list = re.compile(r"(\w*): \[")
 
 re_multiline_list = re.compile(
     r"(?P<blockname>\w*) = {(?P<words>((?:\n\s{0,})(\w*)){1,})\s{1,}}",
     re.MULTILINE
 )
+re_simple_word = re.compile(r"\b(([a-zA-Z_\/]{2,}))\b")
 
+COMMA = ','
+
+""" Code """
 def clean_up_line(line: str) -> str:
-    """ Handle a variety of line contents """
-    # Open a block
-    # if line_opens_block := re.match(block_open, line):
-    #     return 
+    """ Handle a variety of line contents
+    This is the main method that crunches any given line from a traits file
+    """
+
+    # Wipe out any comments
+    if '#' in line:
+        line = scrub_comments_from_line(line)
 
     line_opens_block = '= {' in line
     # ignore multiple blocks on same line
@@ -42,38 +51,65 @@ def clean_up_line(line: str) -> str:
     elif line_opens_block and line.strip().endswith('}'):
         return normalize_list(line)
     # beginning of block
-    # elif line.strip().endswith('= {'):
-    #     return convert_block_open(line)
+
     elif re_block_open := re.match(block_open, line):
         return block_open_to_json(re_block_open)
     elif len(line.strip().split(' ')) == 3:
         return convert_simple_assignment(line)
     elif line.strip() == '}':
-        return line
+        # Add comma
+        return line + COMMA
     elif single_word_result := re.match(re_single_word_line, line):
+        # If it's just one line, return that line ...
         return line
     # Don't munge a list that we crunched already
     elif list_detected := re.search(detect_crunched_list, line):
         return line
+    elif line.strip() == '':
+        # Pass whitespace, deal with it later
+        return line
     else:
         raise WhatInTheShroud(line)
 
-
 def block_open_to_json(re_results: re.Match) -> str:
-    return f"{re_results.group('indent')}{re_results.group('name')}: {{"
+    return f"{re_results.group('indent')}\"{re_results.group('name')}\": {{"
 
-def convert_block_open(line):
-    return line.replace(' = {',': {')
+def convert_block_open(line) -> str:
+    # To be run on a single line only
+    quoted = re.sub(
+        re_simple_word,
+        quote_word,
+        line
+    )
+    return quoted.replace(
+        ' = {',
+        ': {'
+    )
 
-def normalize_list(line):
+def normalize_list(line) -> str:
     # leader_class = { word word word } into leader_class
     parsed = re.search(parse_list_assignment, line)
     indent = re.match(indentation, line).group("indent")
     items = parsed.group("items").strip().split(' ')
-    return f"{indent}{parsed.group('assignee')}: {items}"
+    return f"{indent}\"{parsed.group('assignee')}\": {items}" + COMMA
 
-def convert_simple_assignment(line):
-    return line.replace(' =',':')
+def convert_simple_assignment(line) -> str:
+    quoted = re.sub(
+        re_simple_word,
+        quote_word,
+        line
+    )
+    return quoted.replace(' =',':') + COMMA
+
+def scrub_comments_from_line(line) -> str:
+    # Have already done the check for '#' outside of this method
+    line_parts = line.split('#')
+    # doesn't matter if this is whitespace, we can't return None
+    # return to the left of the comment, minus whitespace on the right side of that part
+    return line_parts[0].rstrip()
+
+def quote_word(word_match: str):
+    return f"\"{word_match.group(0)}\""
 
 def to_json(lines):
     """ Iterate some lines and pray to the Shroud to convert to JSON """
@@ -98,8 +134,18 @@ def compress_list_result_from_search(search_results) -> str:
     words_list = words_raw.strip().split("\n")
     cleaned_words = [ word.strip() for word in words_list ]
     cleaned_words_done = ",".join(cleaned_words)
-    return f"{search_results.group('blockname')}: {str(cleaned_words)}"
+    return f"\"{search_results.group('blockname')}\": {str(cleaned_words)},"
 
+def iter_clean_up_lines(lines: list[str]) -> str:
+    """ Iterate lines and return JSON as text in a single blob """
+    processed_lines = []
+    for line in lines:
+
+        processed_lines.append(clean_up_line(line))
+    
+    # return "\n"
+
+""" Exception Classes """
 class CzOneLiner(Exception):
     pass
 
--- mre_code_tools/cz2json/tests/test_converter.py
+++ mre_code_tools/cz2json/tests/test_converter.py
@@ -2,12 +2,14 @@ from unittest import TestCase
 import re
 
 from ..converter import (
-    normalize_list,
-    convert_block_open,
-    clean_up_line,
-    convert_simple_assignment,
     block_open_to_json,
     block_open,
+    clean_up_line,
+    convert_block_open,
+    convert_simple_assignment,
+    MultipleBlocksSameLine,
+    normalize_list,
+    scrub_comments_from_line,
     search_blob_crunch_lists,
 )
 
@@ -15,8 +17,8 @@ class TestConverter(TestCase):
 
     def test_normalize_list(self):
 
-        line = "	leader_class = { commander scientist }"
-        expectation = "	leader_class: ['commander', 'scientist']"
+        line = "    leader_class = { commander scientist }"
+        expectation = "    \"leader_class\": ['commander', 'scientist'],"
         actual = normalize_list(line)
         assert expectation == actual
         
@@ -26,7 +28,7 @@ class TestConverter(TestCase):
 
     def test_block_open(self):
         line = "	leader_class = {"
-        expectation = "	leader_class: {"
+        expectation = "	\"leader_class\": {"
         actual = convert_block_open(line)
         assert expectation == actual
         
@@ -35,7 +37,7 @@ class TestConverter(TestCase):
         
     def test_block_open_json(self):
         line = "    leader_class = {"
-        expectation = "    leader_class: {"
+        expectation = "    \"leader_class\": {"
         re_match = re.match(block_open, line)
         actual = block_open_to_json(re_match)
         assert expectation == actual
@@ -43,7 +45,7 @@ class TestConverter(TestCase):
 
     def test_simple_assign(self):
         line = "         has_trait = subclass_commander_admiral"
-        expectation = "         has_trait: subclass_commander_admiral"
+        expectation = "         \"has_trait\": \"subclass_commander_admiral\","
         actual = convert_simple_assignment(line)
         actual2 = clean_up_line(line)
         assert expectation == actual
@@ -51,7 +53,7 @@ class TestConverter(TestCase):
     
     def test_touching_numbers(self):
         line = "fleet_mia_time_mult = -0.1"
-        expectation = "fleet_mia_time_mult: -0.1"
+        expectation = "\"fleet_mia_time_mult\": -0.1,"
         actual = convert_simple_assignment(line)
         actual2 = clean_up_line(line)
         assert expectation == actual
@@ -66,11 +68,11 @@ class TestConverter(TestCase):
         "}",
         ]
         expectations = [
-        "opposites: {",
+        "\"opposites\": {",
         "    leader_trait_gale_speed",
         "    leader_trait_gale_speed_2",
         "    leader_trait_gale_speed_3",
-        "}",
+        "},",
         ]
         for line, expect in zip(lines, expectations):
             actual = clean_up_line(line)
@@ -98,7 +100,7 @@ class TestConverter(TestCase):
 		is_gestalt = no
 	}
 	leader_class = { commander }
-	opposites: ['leader_trait_gale_speed', 'leader_trait_gale_speed_2', 'leader_trait_gale_speed_3']
+	\"opposites\": ['leader_trait_gale_speed', 'leader_trait_gale_speed_2', 'leader_trait_gale_speed_3'],
 	selectable_weight = {
 		weight = @class_negative_trait_weight
 		inline_script = paragon/pilot_weight_mult
@@ -106,6 +108,40 @@ class TestConverter(TestCase):
 """
         actual = search_blob_crunch_lists(blob)
         assert expected == actual
+    
+    def test_multiple_blocks_same_line(self):
+        # not supporting crunching multiple blocks on the same line,
+        # this should be resolved before processing with a substitution
+        blob = "      leader_potential_add = { owner = { is_gestalt = no } }"
+        with self.assertRaises(MultipleBlocksSameLine):
+            result = clean_up_line(blob)
+
+    def test_scrub_comment_from_line(self):
+        data = "      has_paragon_dlc = no  # some comment"
+        expected = "      has_paragon_dlc = no"
+        actual = scrub_comments_from_line(data)
+        assert expected == actual
+    
+    def test_iterating_a_block(self):
+        lines = [
+            "modifier = {",
+            "    ship_speed_mult = 0.05",
+            "    ship_hyperlane_range_add = 2",
+            "    fleet_mia_time_mult = -0.1",
+            "}",
+        ]
+
+        expectations = [
+            "\"modifier\": {",
+            "    \"ship_speed_mult\": 0.05,",
+            "    \"ship_hyperlane_range_add\": 2,",
+            "    \"fleet_mia_time_mult\": -0.1,",
+            "},",
+        ]
+        for line, expect in zip(lines, expectations):
+            actual = clean_up_line(line)
+            assert actual == expect
+
 
 test_data = """
 leader_trait_scout = {
