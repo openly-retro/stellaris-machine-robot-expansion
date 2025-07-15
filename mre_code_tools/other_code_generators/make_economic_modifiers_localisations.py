d7fa6af94f3097810eb8e8767f44a326e3ffb0e6
Openly Retro <3236094+openly-retro@users.noreply.github.com>
2025-07-15 01:02:12 -0400

    Batch of minor changes, stopping here bc i'm tired #443

--- mre_code_tools/other_code_generators/make_economic_modifiers_localisations.py
+++ mre_code_tools/other_code_generators/make_economic_modifiers_localisations.py
@@ -1,3 +1,4 @@
+from typing import List
 from cz2json.converter import input_cz_output_json
 from pipeline.mre_common_vars import GAME_RESOURCES
 
@@ -10,9 +11,13 @@ generate loc strings for all create all possible permutations, ex:
 
 from xvcv_mdlc_world_machines_cooling_districts, generate_mult_modifiers, upkeep
 
-mod_xvcv_mdlc_world_machines_cooling_districts_upkeep_mult: "$xvcv_mdlc_world_machines_cooling_districts$ upkeep:"
-mod_xvcv_mdlc_world_machines_cooling_districts_minerals_upkeep_mult: "$xvcv_mdlc_world_machines_cooling_districts$ £minerals£ upkeep:"
+mod_xvcv_mdlc_world_machines_cooling_districts_upkeep_mult: "$xvcv_mdlc_world_machines_cooling_districts$ upkeep"
+mod_xvcv_mdlc_world_machines_cooling_districts_minerals_upkeep_mult: "$xvcv_mdlc_world_machines_cooling_districts$ £minerals£ upkeep"
 etc.
 """
 
+district_mod_with_resource = "mod_{district}_{resource}_{mod_type}: \"${district}$ ${mod_value}$\""
+district_mod_without_resource = "mod_{district}_{mod_type}: \"${district}$ ${mod_value}$\""
 
+def delve_through_categories(economic_cats_dict: dict) -> List[str]:
+    1
